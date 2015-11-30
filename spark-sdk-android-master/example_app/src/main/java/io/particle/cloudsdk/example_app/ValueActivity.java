8e0070cfb47c6b55f0e0b023d6f94649827179b9
shrike92 <far.zodd@gmail.com>
2015-11-30 02:06:34 -0800

    got delay and steps working.  new tinker fork with new function

--- spark-sdk-android-master/example_app/src/main/java/io/particle/cloudsdk/example_app/ValueActivity.java
+++ spark-sdk-android-master/example_app/src/main/java/io/particle/cloudsdk/example_app/ValueActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.io.IOException;
@@ -23,6 +24,10 @@ public class ValueActivity extends AppCompatActivity {
     private static final String ARG_DEVICEID = "ARG_DEVICEID";
 
     private TextView tv;
+    private EditText stepDelayVal;
+    private EditText numStepsVal;
+    private char[] intermediateMsg = new char[40];
+    private String commandMsg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,8 +35,11 @@ public class ValueActivity extends AppCompatActivity {
         setContentView(R.layout.activity_value);
         tv = (TextView) findViewById(R.id.value);
         tv.setText(String.valueOf(getIntent().getIntExtra(ARG_VALUE, 0)));
+        stepDelayVal = (EditText) findViewById(R.id.stepDelay);
+        numStepsVal = (EditText) findViewById(R.id.numSteps);
 
-        findViewById(R.id.refresh_button).setOnClickListener(new View.OnClickListener() {
+
+        findViewById(R.id.led_on).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //...
@@ -41,42 +49,29 @@ public class ValueActivity extends AppCompatActivity {
                     public Object callApi(ParticleCloud ParticleCloud) throws ParticleCloudException, IOException {
                         ParticleDevice device = ParticleCloud.getDevice(getIntent().getStringExtra(ARG_DEVICEID));
                         Object variable;
-                        int writeResponse;
-                        try {
-                            for (String funcName : device.getFunctions()) {
-                                Log.i("SOME_TAG", "Device has function: " + funcName);
-                            }
-                            variable = device.getVariable("analogvalue");
+                        int curIndex, splitIndex;
+                        int speed = Integer.parseInt(stepDelayVal.getText().toString());
+                        int steps = Integer.parseInt(numStepsVal.getText().toString());
 
-                        } catch (ParticleDevice.VariableDoesNotExistException e) {
-                            //Toaster.l(ValueActivity.this, e.getMessage());
-                            variable = -1;
+                        for (curIndex = 0, splitIndex = 0; curIndex < 18 && splitIndex < ((Integer.toString(speed)).toCharArray()).length; curIndex++, splitIndex++){
+                            intermediateMsg[curIndex] = ((Integer.toString(speed)).toCharArray())[splitIndex];
                         }
+                        intermediateMsg[curIndex] = 'f';
 
+                        for (curIndex = curIndex+1,splitIndex = 0; curIndex < 38 && splitIndex < ((Integer.toString(steps)).toCharArray()).length; curIndex++, splitIndex++){
+                            intermediateMsg[curIndex] = ((Integer.toString(steps)).toCharArray())[splitIndex];
+                        }
+                        intermediateMsg[curIndex] = 'g';
+
+                        commandMsg = new String(intermediateMsg);
+                        Log.d("messagecheck",commandMsg);
                         try {
-                            Boolean isHigh;
-                            int currentState = device.callFunction("digitalread", Py.list("D7"));
-                            Log.d("StateOfPin","Current value: " + Integer.toString(currentState));
-                            if (currentState != 0){ isHigh = Boolean.TRUE;}
-                            else {isHigh = Boolean.FALSE;}
-
-                            if (isHigh){
-                                writeResponse = device.callFunction("digitalwrite", Py.list("D7", "LOW"));
-                                variable = "Set to LOW";
-                            }
-                            else{
-                                writeResponse = device.callFunction("digitalwrite", Py.list("D7", "HIGH"));
-                                variable = "Set to HIGH";
-                            }
-                            //String tinkerResp = Integer.toString(writeResponse);
-                            //Log.d("BANANA", "analogvalue: " + obj);
-                            //Toaster.s(ValueActivity.this, tinkerResp);
-                            //variable = "Set to HIGH";
+                            device.callFunction("movestepper", Py.list(commandMsg));
+                            variable = "Set to HIGH";
                         } catch (ParticleDevice.FunctionDoesNotExistException e) {
                             Toaster.s(ValueActivity.this, "Function doesn't exist.");
                             variable = "Func not found";
                         }
-
                         return variable;
                     }
 
@@ -92,6 +87,43 @@ public class ValueActivity extends AppCompatActivity {
                 });
             }
         });
+
+        findViewById(R.id.led_off).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //...
+                // Do network work on background thread
+                Async.executeAsync(ParticleCloud.get(ValueActivity.this), new Async.ApiWork<ParticleCloud, Object>() {
+                    @Override
+                    public Object callApi(ParticleCloud ParticleCloud) throws ParticleCloudException, IOException {
+                        ParticleDevice device = ParticleCloud.getDevice(getIntent().getStringExtra(ARG_DEVICEID));
+                        Object variable;
+                        int speed = Integer.parseInt(stepDelayVal.getText().toString());
+                        int steps = Integer.parseInt(numStepsVal.getText().toString());
+
+                        try {
+                            device.callFunction("digitalwrite", Py.list("D7", "LOW"));
+                            variable = "Set to LOW";
+                        } catch (ParticleDevice.FunctionDoesNotExistException e) {
+                            Toaster.s(ValueActivity.this, "Function doesn't exist.");
+                            variable = "Func not found";
+                        }
+                        return variable;
+                    }
+
+                    @Override
+                    public void onSuccess(Object i) { // this goes on the main thread
+                        tv.setText(i.toString());
+                    }
+
+                    @Override
+                    public void onFailure(ParticleCloudException e) {
+                        e.printStackTrace();
+                    }
+                });
+            }
+        });
+
     }
 
     public static Intent buildIntent(Context ctx, Integer value, String deviceid) {
